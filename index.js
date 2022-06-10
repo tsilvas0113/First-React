d5797d80c1d4e6da4eb1aeef0d855b3cac83c60e
Tristan <tsilvas0113@gmail.com>
2022-06-10 18:04:30 -0500

    Concept of Declarative

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-function MainContent () {
-    return (
-        <h1>I'm learning React!</h1>
-    )
-}
+//Imperative way - telling how to do something
 
-ReactDOM.render(
-    <div>
-        <MainContent />
-    </div>,
+const header = document.createElement('h1')
+header.textContent = "Buddy's ears are powerful"
+header.className = 'header'
+
+document.getElementById('root').append(header)
+
+//Declarative way - telling what to do
+
+ReactDOM.render(<h1 className="header">Buddy's ears are powerful</h1>,
     document.getElementById('root')
 )
