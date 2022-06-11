a297f5150b9f877668a2c3e5a7c8bf01ab1f27bd
Tristan <tsilvas0113@gmail.com>
2022-06-11 11:45:57 -0500

    Concept of Custom Components II

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function NewPage () {
     return (
-        <ol>
-            <li>React is cool</li>
-            <li>React can get me hired</li>
-            <li>React can make great looking sites</li>
-        </ol>
+        <div>
+            <header>
+                <nav>
+                    <img src='./react-logo.png' width='50px' />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>React is cool</li>
+                <li>React can get me hired</li>
+                <li>React can make great looking sites</li>
+            </ol>
+            <footer>
+                <small>© 2022 Silvas development. All rights reserved.</small>
+            </footer>
+        </div>
     )
 }
 
